c39f267b0d7b2e2b366877516b15376e8a9a87e4
Lucas Daniel <101567841+lucasdanielzx@users.noreply.github.com>
2022-06-23 15:26:49 -0300

    Add files via upload

---
+++ Projeto Remoto (II Unidade)/bibliotecaPR.sql
@@ -0,0 +1,108 @@
+use biblioteca;
+
+create database biblioteca;
+
+-- CRIAÇÃO DAS TABELAS:
+
+CREATE TABLE BIBLIOTECA (
+	idBiblioteca INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomeBiblioteca VARCHAR (60) NOT NULL,
+    tipoBiblioteca VARCHAR (15) NOT NULL,
+    idEndereco INT,
+    constraint fk_idEndereco_Biblioteca foreign key (idEndereco)
+		references ENDERECO (idEndereco)
+);
+
+CREATE TABLE ENDERECO (
+	idEndereco INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    bairroEndereco VARCHAR (45) NOT NULL,
+    ruaEndereco VARCHAR (45) NOT NULL,
+    numeroEndereco INT NOT NULL,
+    cepEndereco VARCHAR (10) NOT NULL,
+    cidadeEndereco VARCHAR (20) NOT NULL
+);
+    
+CREATE TABLE TELEFONE (
+	idTelefone INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dddTelefone INT NOT NULL,
+    numeroTelefone INT NOT NULL,
+    idBiblioteca INT,
+    constraint fk_idBiblioteca_Telefone foreign key (idBiblioteca)
+		references BIBLIOTECA (idBiblioteca),
+	idFuncionario INT, 
+    constraint fk_idFuncionario_Telefone foreign key (idFuncionario)
+		references FUNCIONARIO (idFuncionario),
+	idUsuario INT,
+    constraint fk_idUsuario_Telefone foreign key (idUsuario)
+		references USUARIO (idUsuario)
+);
+
+CREATE TABLE FUNCIONARIO (
+	idFuncionario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomeFuncionario VARCHAR (45) NOT NULL,
+    cpfFuncionario VARCHAR (15) NOT NULL,
+    idBiblioteca INT,
+    constraint fk_idBiblioteca_Funcionario foreign key (idBiblioteca)
+		references BIBLIOTECA (idBiblioteca),
+    idEndereco INT,
+    constraint fk_idEndereco_Funcionario foreign key (idEndereco)
+		references ENDERECO (idEndereco)
+);
+
+CREATE TABLE USUARIO (
+	idUsuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomeUsuario VARCHAR (45) NOT NULL,
+    cpfUsuario VARCHAR (15) NOT NULL, 
+    emailUsuario VARCHAR (45) NOT NULL,
+    idEndereco INT,
+    constraint fk_idEndereco_Usuario foreign key (idEndereco)
+		references ENDERECO (idEndereco)
+);
+
+CREATE TABLE LIVRO (
+	idLivro INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomeLivro VARCHAR (45) NOT NULL,
+    anoPublicacao VARCHAR (10) NOT NULL,
+    categoriaLivro VARCHAR (20) NOT NULL,
+    idEditora INT,
+    constraint fk_idEditora_Livro foreign key (idEditora) 
+		references EDITORA (idEditora),
+	idCadastroLocacao INT,
+    constraint fk_idCadastroLocacao_Livro foreign key (idCadastroLocacao)
+		references CADASTRO_LOCACAO (idCadastroLocacao)
+);
+
+CREATE TABLE AUTOR (
+	idAutor INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomeAutor VARCHAR (45) NOT NULL,
+    cpfAutor VARCHAR (15) NOT NULL
+);
+
+CREATE TABLE AUTOR_HAS_LIVRO (
+	idAutor INT,
+    constraint fk_idAutor_AutorHasLivro foreign key (idAutor)
+		references AUTOR (idAutor),
+	idLivro INT,
+    constraint fk_idLivro_AutorHasLivro foreign key (idLivro)
+		references LIVRO (idLivro)
+);
+
+CREATE TABLE EDITORA (
+	idEditora INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nomeEditora VARCHAR (45) NOT NULL,
+    cnpjEditora VARCHAR (20) NOT NULL,
+    emailEditora VARCHAR (45) NOT NULL
+);
+
+CREATE TABLE CADASTRO_LOCACAO (
+	idCadastroLocacao INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dataLocacao VARCHAR (10) NOT NULL,
+    dataDevolucao VARCHAR (10) NOT NULL,
+    descricaoLocacao VARCHAR (60) NOT NULL,
+    idUsuario INT,
+    constraint fk_idUsuario_CadastroLocacao foreign key (idUsuario)
+		references USUARIO (idUsuario),
+	idFuncionario INT,
+    constraint fk_idFuncionario_CadastroLocacao foreign key (idFuncionario)
+		references FUNCIONARIO (idFuncionario)
+);
