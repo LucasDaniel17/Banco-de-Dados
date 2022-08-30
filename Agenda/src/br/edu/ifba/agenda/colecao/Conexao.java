9ff4e6fcca5ffcb6f150417b90446460866324c5
Lucas Daniel <101567841+lucasdanielzx@users.noreply.github.com>
2022-08-30 12:23:57 -0300

    Add files via upload

--- Agenda/src/br/edu/ifba/agenda/colecao/Conexao.java
+++ Agenda/src/br/edu/ifba/agenda/colecao/Conexao.java
@@ -12,7 +12,7 @@ public class Conexao {
 	
 	static {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 		} catch(Exception e) {
 			System.out.println("ERRO: " + e.getMessage());
 		}
